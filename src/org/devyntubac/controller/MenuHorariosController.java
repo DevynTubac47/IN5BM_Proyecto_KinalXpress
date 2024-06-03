37e1fe4f275b47593dfa52a5f93e23d90b76d96d
DevynTubac47 <dtubac-2020247@kinal.edu.gt>
2024-06-03 17:07:29 -0600

    Update

--- src/org/devyntubac/controller/MenuHorariosController.java
+++ src/org/devyntubac/controller/MenuHorariosController.java
@@ -1,30 +1,3 @@
-package org.devyntubac.controller;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
-import org.devyntubac.system.Main;
-
-/**
- * FXML Controller class
- *
- * @author devyn
- */
-public class MenuHorariosController implements Initializable {
-
-    private Main escenarioPrincipal;
-
-    public Main getEscenarioPrincipal() {
-        return escenarioPrincipal;
-    }
-
-    public void setEscenarioPrincipal(Main escenarioPrincipal) {
-        this.escenarioPrincipal = escenarioPrincipal;
-    }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:11a504eda651fe422414bae154eb9621c5123ed7996ea1bd9666d52190eeacff
+size 663
